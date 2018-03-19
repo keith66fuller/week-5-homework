81bc4a644309d50cf36650b7000b6f6afb588d33
Keith Fuller <36208707+keith66fuller@users.noreply.github.com>
2018-03-19 14:58:51 -0500

    Made giphy API call https on port 443.

--- assets/javascript/giphyImages.js
+++ assets/javascript/giphyImages.js
@@ -10,7 +10,7 @@ $('document').ready( function() {
     ];
 
     $.ajax({
-        url: "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
+        url: "https://api.wordnik.com:443/v4/words.json/randomWords?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
         method: 'GET',
     })
     .done(function(response) {
